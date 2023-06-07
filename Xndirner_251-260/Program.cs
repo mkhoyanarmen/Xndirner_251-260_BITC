6ab8f43734dd8f6799fba9b4515bda1addfb8905
mkhoyanarmen <93216230+mkhoyanarmen@users.noreply.github.com>
2023-06-07 19:54:57 +0400

    Add project files.

---
+++ Xndirner_251-260/Program.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xndirner_251_260
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //251
+            //Random rnd = new Random();
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] arr = new int[n];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = rnd.Next(-10, 20);
+            //    Console.Write(arr[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int max = arr[0];
+            //for (int i = 1; i < arr.Length; i++)
+            //{
+            //    if (max < arr[i])
+            //    {
+            //        max = arr[i];
+            //    }
+            //}
+            //Console.WriteLine($"Max: {max}");
+
+            //252
+            //Random rnd = new Random();
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] arr = new int[n];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = rnd.Next(-10, 20);
+            //    Console.Write(arr[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int min = arr[0];
+            //for (int i = 1; i < arr.Length; i++)
+            //{
+            //    if (min > arr[i])
+            //    {
+            //        min = arr[i];
+            //    }
+            //}
+            //Console.WriteLine($"Min: {min}");
+
+            //253
+            //Random rnd = new Random();
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] arr = new int[n];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = rnd.Next(-10, 20);
+            //    Console.Write(arr[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int max = arr[0];
+            //int min = arr[0];
+            //for (int i = 1; i < arr.Length; i++)
+            //{
+            //    if (max < arr[i])
+            //    {
+            //        max = arr[i];
+            //    }
+            //    if (min > arr[i])
+            //    {
+            //        min = arr[i];
+            //    }
+            //}
+            //Console.WriteLine($"Max: {max}\nMin: {min}\nSum: {min + max}");
+
+            //254
+            //Random rnd = new Random();
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] arr = new int[n];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = rnd.Next(-10, 20);
+            //    Console.Write(arr[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int max = arr[0];
+            //int min = arr[0];
+            //for (int i = 1; i < arr.Length; i++)
+            //{
+            //    if (max < arr[i])
+            //    {
+            //        max = arr[i];
+            //    }
+            //    if (min > arr[i])
+            //    {
+            //        min = arr[i];
+            //    }
+            //}
+            //Console.WriteLine($"Max: {max}\nMin: {min}\nSum: {min * max}");
+
+            //255
+            //Random rnd = new Random();
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] arr = new int[n];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = rnd.Next(-10, 20);
+            //    Console.Write(arr[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int max = arr[0], ind = 0;
+            //for (int i = 1; i < arr.Length; i++)
+            //{
+            //    if (max < arr[i])
+            //    {
+            //        max = arr[i];
+            //        ind = i;
+            //    }
+            //}
+            //Console.WriteLine($"Max + index: {max + ind}");
+
+            //256
+            //Random rnd = new Random();
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] arr = new int[n];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = rnd.Next(-10, 20);
+            //    Console.Write(arr[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int min = arr[0], ind = 0;
+            //for (int i = 1; i < arr.Length; i++)
+            //{
+            //    if (min > arr[i])
+            //    {
+            //        min = arr[i];
+            //        ind = i;
+            //    }
+            //}
+            //Console.WriteLine($"Min + index: {min + ind}");
+
+            //257
+            //Random rnd = new Random();
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] arr = new int[n];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = rnd.Next(-10, 20);
+            //    Console.Write(arr[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int max = arr[0];
+            //for (int i = 1; i < arr.Length; i++)
+            //{
+            //    if (max < arr[i])
+            //    {
+            //        max = arr[i];
+            //    }
+            //}
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] == max)
+            //    {
+            //        Console.WriteLine($"Index: {i}");
+            //        break;
+            //    }
+            //}
+
+            //258
+            //Random rnd = new Random();
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] arr = new int[n];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = rnd.Next(-10, 20);
+            //    Console.Write(arr[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int max = arr[0];
+            //for (int i = 1; i < arr.Length; i++)
+            //{
+            //    if (max < arr[i])
+            //    {
+            //        max = arr[i];
+            //    }
+            //}
+
+            //for (int i = arr.Length - 1; i > 0; i--)
+            //{
+            //    if (arr[i] == max)
+            //    {
+            //        Console.WriteLine($"Index: {i}");
+            //        break;
+            //    }
+            //}
+
+            //259
+            //Random rnd = new Random();
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] arr = new int[n];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = rnd.Next(-10, 20);
+            //    Console.Write(arr[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int min = arr[0];
+            //for (int i = 1; i < arr.Length; i++)
+            //{
+            //    if (min > arr[i])
+            //    {
+            //        min = arr[i];
+            //    }
+            //}
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] == min)
+            //    {
+            //        Console.WriteLine($"Index: {i}");
+            //        break;
+            //    }
+            //}
+
+            //260
+            Random rnd = new Random();
+            int n;
+            do
+            {
+                Console.WriteLine("Mutqagrel zangvaci chapy");
+                n = int.Parse(Console.ReadLine());
+            } while (!(n > 0));
+
+            int[] arr = new int[n];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(-10, 20);
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+
+            int min = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (min > arr[i])
+                {
+                    min = arr[i];
+                }
+            }
+
+            for (int i = arr.Length - 1; i > 0; i--)
+            {
+                if (arr[i] == min)
+                {
+                    Console.WriteLine($"Index: {i}");
+                    break;
+                }
+            }
+        }
+    }
+}
